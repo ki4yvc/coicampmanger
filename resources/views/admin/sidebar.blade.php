9c6c5c6c5cb6bb579010bf048c4fba7f5c331db1
= <ki4yvc@gmail.com>
2016-02-17 10:33:36 -0500

    Updates to the admin panel

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -10,24 +10,27 @@
       </div>
       <div class="pull-left info">
         <p>{{ Auth::user()->name }}</p>
-        <!-- Status -->
-        <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
     </div>
-
+    <br>
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
-      <li class="header">HEADER</li>
+      <li class="header">Functions</li>
       <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-      <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+      <li class="active"><a href="#"><i class="fa fa-home"></i> <span>Home</span></a></li>
       <li class="treeview">
-        <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#"><i class="fa fa-calendar-check-o"></i> <span>Registration</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
-          <li><a href="#">Link in level 2</a></li>
-          <li><a href="#">Link in level 2</a></li>
+          <li><a href="#">Week 1</a></li>
+          <li><a href="#">Week 2</a></li>
+          <li><a href="#">Week 3</a></li>
+          <li><a href="#">Week 4</a></li>
+          <li><a href="#">Week 5</a></li>
+          <li><a href="#">Week 6</a></li>
+          <li><a href="#">Week 7</a></li>
         </ul>
       </li>
+      <li><a href="#"><i class="fa fa-gears"></i> <span>Setup</span></a></li>
     </ul><!-- /.sidebar-menu -->
   </section>
   <!-- /.sidebar -->
