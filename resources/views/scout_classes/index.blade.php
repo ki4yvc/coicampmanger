a285ee02d9e6f88b319c83fbf843c1a0a3eeff86
= <ki4yvc@gmail.com>
2016-02-18 16:13:05 -0500

    Finilizes needed scout_class fuctions and view

--- resources/views/scout_classes/index.blade.php
+++ resources/views/scout_classes/index.blade.php
@@ -5,7 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Camper, Johny - <string>16 Years Old</strong></div>
+                <div class="panel-heading">
+                    Camper, Johny - <string>16 Years Old:</strong>
+                    <a href="{{ URL::to('#') }}"><i class="fa fa-edit"> Edit Schedule</i></a> |
+                    <a href="{{ URL::to('#') }}"><i class="fa fa-print"> Print Schedule</i></a> |
+                    <a href="{{ URL::to('#') }}"><i class="fa fa-user"> Edit Scout</i></a>
+                  </div>
                   <div class="panel-body">
                       <table class="table table-hover">
                         <thead>
@@ -21,19 +26,38 @@
                         <tbody>
                           <tr>
                             <td>9:00am-12:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                           <tr>
                             <td>2:000pm-5:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                           <tr>
                             <td>7:00pm-9:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                         </tbody>
                       </table>
                   </div>
             </div>
             <div class="panel panel-default">
-                <div class="panel-heading">Wilcox, Dylan - <string>17 Years Old</strong></div>
+                <div class="panel-heading">Wilcox, Dylan - <string>17 Years Old</strong>
+                  <a href="{{ URL::to('#') }}"><i class="fa fa-edit"> Edit Schedule</i></a> |
+                  <a href="{{ URL::to('#') }}"><i class="fa fa-print"> Print Schedule</i></a> |
+                  <a href="{{ URL::to('#') }}"><i class="fa fa-user"> Edit Scout</i></a>
+                </div>
                   <div class="panel-body">
                       <table class="table table-hover">
                         <thead>
@@ -49,12 +73,27 @@
                         <tbody>
                           <tr>
                             <td>9:00am-12:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                           <tr>
                             <td>2:000pm-5:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                           <tr>
                             <td>7:00pm-9:00pm</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
+                            <td>Free</td>
                           </tr>
                         </tbody>
                       </table>
